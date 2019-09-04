59e4ada0ea7e66682b17979b72301e4e3667dad2
Sebster101 <44649965+Sebster101@users.noreply.github.com>
2019-09-04 11:32:36 -0700

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,14 @@
+Sebastian Ludlow
+Hello World Program
+ID: 2290555
+
+Operation:
+Prints out "Hello World" to console
+
+Contents:
+hello.cpp
+Hello.out
+README.txt
+
+References:
+
---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include  <iostream>
+using namespace std;
+//main method
+int main(){
+	//prints hello world
+	cout << "Hello World";
+	//allows console to stay open
+	cin.get();
+	//returns
+	return 0;
+}
+
